403aa3c91404b63bd327efe0f784aa5ef3e683e7
smartwa <smartwacaleb@gmail.com>
2024-06-30 04:22:01 +0300

    feat: Remove save and load handlers functionality

--- bot.py
+++ bot.py
@@ -178,12 +178,6 @@ class IsAdminFilter(SimpleCustomFilter):
 
 bot.add_custom_filter(IsAdminFilter())
 
-bot.enable_save_next_step_handlers(delay=2)
-
-# Load next_step_handlers from save file (default "./.handlers-saves/step.save")
-# WARNING It will work only if enable_save_next_step_handlers was called!
-bot.load_next_step_handlers()
-
 if __name__ == "__main__":
     print("Infinity polling ...")
     bot.infinity_polling()
